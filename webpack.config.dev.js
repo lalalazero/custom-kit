a9263e71d0d82a61df01579593e081dcc9d1d3db
lalalazero <zzzero520@hotmail.com>
2020-07-10 23:00:39 +0800

    feat: setup webpack config and project template

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: "./App.tsx"
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.s([ac])ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'custom-kit',
+            template: './public/index.html',
+        })
+    ]
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+const path = require('path')
+module.exports = {
+    entry: {
+        index: './index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/components'),
+        library: 'custom-kit',
+        libraryTarget: 'umd',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.s([ac])ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    mode: 'production',
+    // 不打包 react，使 production 模式下打包出来的代码变小
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        }
+    }
+}
