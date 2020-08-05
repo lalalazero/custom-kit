20dfe8bdd99ade61b43e8ccc93774b485da50519
lalalazero <zzzero520@hotmail.com>
2020-08-06 00:14:47 +0800

    try waterfall with grid fail

--- demos/ImageViewerDemo.jsx
+++ demos/ImageViewerDemo.jsx
@@ -9,6 +9,8 @@ const Demo = <Waterfall>
     <div className="item-2">2</div>
     <div className='item-3'>3</div>
     <div className="item-4">4</div>
+    <div className='item-1'>5</div>
+    <div className="item-2">6</div>
 </Waterfall>
 
 export default Demo 
