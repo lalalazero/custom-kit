2961a0a22580e0ebe036060442a0a9d3e36dbc09
lalalazero <zzzero520@hotmail.com>
2020-08-06 00:45:53 +0800

    waterfall with coumn-count css property works.

--- components/Waterfall/index.tsx
+++ components/Waterfall/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './index.scss'
 
+
 export default class Waterfall extends Component {
     constructor(props: {}) {
         super(props)
--- demos/ImageViewerDemo.jsx
+++ demos/ImageViewerDemo.jsx
@@ -5,12 +5,12 @@ import './ImageViewerDemo.scss'
 
 
 const Demo = <Waterfall>
-    <div className='item-1'>1</div>
-    <div className="item-2">2</div>
-    <div className='item-3'>3</div>
-    <div className="item-4">4</div>
-    <div className='item-1'>5</div>
-    <div className="item-2">6</div>
+    <div className='item-1 item'>1</div>
+    <div className="item-2 item">2</div>
+    <div className='item-3 item'>3</div>
+    <div className="item-4 item">4</div>
+    <div className='item-1 item'>5</div>
+    <div className="item-2 item">6</div>
 </Waterfall>
 
 export default Demo 
